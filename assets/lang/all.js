9df82781f580334314c375cb458aff1adc8e040e
semihkagan <97614413+semihkagan@users.noreply.github.com>
2024-07-15 11:13:45 +0300

    Add files via upload

---
+++ assets/lang/all.js
@@ -0,0 +1,203 @@
+// LAST UPDATE FİLE , FILE IS NOT RUNABLE
+/* 
+İngilizce (en)
+Türkçe (tr)
+Fransızca (fr)
+İspanyolca (es)
+Almanca (de)
+İtalyanca (it)
+Portekizce (pt)
+Hollandaca (nl)
+Rusça (ru)
+Çince (zh)
+Japonca (ja)
+Korece (ko)
+Arapça (ar)
+Hintçe (hi)
+Yunanca (el)
+Lehçe (pl)
+Çekçe (cs)
+Macarca (hu)
+Tayca (th)
+Vietnamca (vi)
+İsveççe (sv)
+Danca (da)
+Fince (fi)
+Norveççe (no)
+Endonezce (id)
+Malayca (ms)
+Romence (ro)
+Ukraynaca (uk)
+Bulgarca (bg)
+Litvanca (lt)
+Letonca (lv)
+Estonca (et)
+*/
+
+const translations = {
+    en: {
+        button: "Copy",
+        buttonCopied: "Copied!",
+        title: "Your IPv6 Address"
+    },
+    tr: {
+        button: "Kopyala",
+        buttonCopied: "Kopyalandı!",
+        title: "IPv6 Adresiniz"
+    },
+    fr: {
+        button: "Copier",
+        buttonCopied: "Copié !",
+        title: "Votre adresse IPv6"
+    },
+    es: {
+        button: "Copiar",
+        buttonCopied: "¡Copiado!",
+        title: "Su dirección IPv6"
+    },
+    de: {
+        button: "Kopieren",
+        buttonCopied: "Kopiert!",
+        title: "Ihre IPv6-Adresse"
+    },
+    it: {
+        button: "Copia",
+        buttonCopied: "Copiato!",
+        title: "Il tuo indirizzo IPv6"
+    },
+    pt: {
+        button: "Copiar",
+        buttonCopied: "Copiado!",
+        title: "Seu endereço IPv6"
+    },
+    nl: {
+        button: "Kopiëren",
+        buttonCopied: "Gekopieerd!",
+        title: "Uw IPv6-adres"
+    },
+    ru: {
+        button: "Копировать",
+        buttonCopied: "Скопировано!",
+        title: "Ваш IPv6-адрес"
+    },
+    zh: {
+        button: "复制",
+        buttonCopied: "复制成功！",
+        title: "您的IPv6地址"
+    },
+    ja: {
+        button: "コピー",
+        buttonCopied: "コピーしました！",
+        title: "あなたのIPv6アドレス"
+    },
+    ko: {
+        button: "복사",
+        buttonCopied: "복사됨!",
+        title: "귀하의 IPv6 주소"
+    },
+    ar: {
+        button: "نسخ",
+        buttonCopied: "تم النسخ!",
+        title: "عنوان IPv6 الخاص بك"
+    },
+    hi: {
+        button: "कॉपी",
+        buttonCopied: "कॉपी किया गया!",
+        title: "आपका IPv6 पता"
+    },
+    tr: {
+        button: "Kopieren",
+        buttonCopied: "Kopiert!",
+        title: "Ihre IPv6-Adresse"
+    },
+    el: {
+        button: "Αντιγραφή",
+        buttonCopied: "Αντιγράφηκε!",
+        title: "Η διεύθυνση IPv6 σας"
+    },
+    pl: {
+        button: "Kopiuj",
+        buttonCopied: "Skopiowano!",
+        title: "Twój adres IPv6"
+    },
+    cs: {
+        button: "Kopírovat",
+        buttonCopied: "Zkopírováno!",
+        title: "Vaše IPv6 adresa"
+    },
+    hu: {
+        button: "Másolás",
+        buttonCopied: "Másolva!",
+        title: "Az IPv6 címéhez"
+    },
+    th: {
+        button: "คัดลอก",
+        buttonCopied: "คัดลอกแล้ว!",
+        title: "ที่อยู่ IPv6 ของคุณ"
+    },
+    vi: {
+        button: "Sao chép",
+        buttonCopied: "Đã sao chép!",
+        title: "Địa chỉ IPv6 của bạn"
+    },
+    sv: {
+        button: "Kopiera",
+        buttonCopied: "Kopierat!",
+        title: "Din IPv6-adress"
+    },
+    da: {
+        button: "Kopier",
+        buttonCopied: "Kopieret!",
+        title: "Din IPv6-adresse"
+    },
+    fi: {
+        button: "Kopioi",
+        buttonCopied: "Kopioitu!",
+        title: "IPv6-osoitteesi"
+    },
+    no: {
+        button: "Kopier",
+        buttonCopied: "Kopiert!",
+        title: "Din IPv6-adresse"
+    },
+    id: {
+        button: "Salin",
+        buttonCopied: "Disalin!",
+        title: "Alamat IPv6 Anda"
+    },
+    ms: {
+        button: "Salin",
+        buttonCopied: "Disalin!",
+        title: "Alamat IPv6 Anda"
+    },
+    ro: {
+        button: "Copiază",
+        buttonCopied: "Copiat!",
+        title: "Adresa dvs. IPv6"
+    },
+    uk: {
+        button: "Копіювати",
+        buttonCopied: "Скопійовано!",
+        title: "Ваш IPv6-адреса"
+    },
+    bg: {
+        button: "Копиране",
+        buttonCopied: "Копирано!",
+        title: "Вашият IPv6 адрес"
+    },
+    lt: {
+        button: "Kopijuoti",
+        buttonCopied: "Nukopijuota!",
+        title: "Jūsų IPv6 adresas"
+    },
+    lv: {
+        button: "Kopēt",
+        buttonCopied: "Kopēts!",
+        title: "Jūsu IPv6 adrese"
+    },
+    et: {
+        button: "Kopeeri",
+        buttonCopied: "Kopeeritud!",
+        title: "Teie IPv6 aadress"
+    },
+};
